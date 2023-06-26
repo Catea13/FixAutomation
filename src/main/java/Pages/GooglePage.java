68e61df2408a79df64291d67d06dd0fd028c695d
cmazur <constancegerman@gmail.com>
2023-06-27 01:03:47 +0300

    Added Google Search button logic

--- src/main/java/Pages/GooglePage.java
+++ src/main/java/Pages/GooglePage.java
@@ -9,15 +9,19 @@ public class GooglePage {
     @FindBy(id = "APjFqb")
     public WebElement searchField;
 
+    @FindBy(className = "gNO89b")
+    public WebElement clickSearchGoogleButton;
 
     public GooglePage(WebDriver driver) {
         this.driver = driver;
     }
 
-
     public void typeInSearchField(String text) {
         searchField.sendKeys(text);
     }
 
+    public void clickSearchButton() {
+        clickSearchGoogleButton.click();
+    }
 
 }
--- src/main/java/Steps/MainSteps.java
+++ src/main/java/Steps/MainSteps.java
@@ -2,6 +2,7 @@ package Steps;
 
 import Hooks.TestContext;
 import Pages.GooglePage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.support.PageFactory;
 
@@ -16,5 +17,18 @@ public class MainSteps {
     public void fillInWith(String value) {
         GooglePage googlePage = PageFactory.initElements(testContext.getHook().driver, GooglePage.class);
         googlePage.typeInSearchField(value);
+    }
+
+    @And("click  {string} button")
+    public void clickButton(String button) {
+        GooglePage googlePage = PageFactory.initElements(testContext.getHook().driver, GooglePage.class);
+        switch (button) {
+            case "Google Search":
+                googlePage.clickSearchButton();
+                break;
+
+            default:
+                System.out.println("This button " + button + " not exists");
+        }
     }
 }
